f4919d0e0e1e9182f58c68c2c7d3347aa5f5895c
Siarhei Kvachonak <s.kvachenok@syberry.com>
2021-01-30 16:45:43 +0300

    Changes for testing

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ function App() {
 This an app for testing Github actions.
         <p>It deploys to S3</p>
         <p>And works after every update</p>
+        <p>Trying as S3 Static Web Hosting</p>
     </div>
   );
 }
